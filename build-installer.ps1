e2bca55128a4538672989cc4320ce12fae5e54a7
cprkv <mnz-rrh@ya.ru>
2022-11-13 15:00:25 +0500

    fix build-installer

--- build-installer.ps1
+++ build-installer.ps1
@@ -1,5 +1,5 @@
 go build -ldflags '-s' -o installer/dep.exe
 cd installer
-rm -r Output
+rm -r Output -ErrorAction SilentlyContinue
 ."C:\Program Files (x86)\Inno Setup 6\Compil32.exe" /cc .\main.iss
 cd ..
